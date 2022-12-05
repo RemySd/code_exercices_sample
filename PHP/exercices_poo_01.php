2d07156ce9390a133d3c97d995cda013264d8663
Remy <remy.sainsard@gmail.com>
2022-12-05 21:31:58 +0100

    Add POO PHP exercice

---
+++ PHP/exercices_poo_01.php
@@ -0,0 +1,7 @@
+<?php 
+
+/* 
+ * Créer une class Animal quand prend 2 proprietés (nom et age)
+ * Créer une class Chien qui va hérité de la class animal. La class Chien aura une fonction qui s'appelera "Parler()" et affichera "Wouaf"
+ * Créer une class Chat qui va hérité de la class animal. La class Chat aura une fonction qui s'appelera "Parler()" et affichera "Miaou"
+ */
